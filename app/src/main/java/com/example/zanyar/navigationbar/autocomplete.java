53ffca232b6f1f173cafd7c48ca85dd486581e1c
endezyar <altunvedat12@gmail.com>
2017-10-20 17:22:17 +0300

    screenshot and autocomplete

---
+++ app/src/main/java/com/example/zanyar/navigationbar/autocomplete.java
@@ -0,0 +1,5 @@
+package com.example.zanyar.navigationbar;
+
+
+public class autocomplete {
+}
---
+++ app/src/main/java/com/example/zanyar/navigationbar/snapshot.java
@@ -0,0 +1,24 @@
+package com.example.zanyar.navigationbar;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+
+public class snapshot extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap mMap;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+}
